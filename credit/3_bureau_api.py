428c01123768984b88ec00013acc61b04481a496
Riley Siebel <riley@chalk.ai>
2022-12-21 14:50:52 -0800

    some credit examples

---
+++ credit/3_bureau_api.py
@@ -0,0 +1,45 @@
+import re
+
+from datetime import datetime
+
+import requests
+from chalk.features import features, feature, has_many, DataFrame
+@features
+class User:
+    id: str
+    first_name: str
+    last_name: str
+    ssn: str = feature(tags="pii")
+    city: str
+    state: str
+    credit_report: DataFrame[CreditReport] \
+        = has_many(lambda: CreditReport.user_id == User.id)
+
+@features
+class CreditReport:
+    report_id: str
+    user_id: str
+    # theres a BUNCH of stuff in here
+    report: json
+
+url: str = None
+@before_all
+def init_credit_vendor():
+    url = os.getenv('MY_VENDOR_URL')
+
+def get_credit_report(first_name: User.first_name, last_name: User.last_name,
+                      city: User.city, state: User.state, id = User.id
+    ) -> CreditReport:
+    args = {
+        "firstName": first_name,
+        "lastName": last_name,
+        "city": city,
+        "state": state
+        }
+    res = requests.post(f"{url}/equifax/credit-report", args).json()
+
+    return CreditReport(
+        user_id=id,
+        report_id=res["pdfReportId"],
+        report=res["data"]
+    )
