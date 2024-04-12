8153b4eb86fd2bd8546352e4e90ee05475e502e4
Samuel Mignot <43255992+sjmignot@users.noreply.github.com>
2024-04-11 17:53:04 -0700

    Update 12_model/1_model.py
    Co-authored-by: Elliot Marx <elliotrmarx@gmail.com>

--- 12_model/1_model.py
+++ 12_model/1_model.py
@@ -2,7 +2,7 @@
 
 In this exmaple, we are using a logistic regression model to predict
 whether a user will churn from our video streaming service. This example
-assumes, we have already trained a simple model on age, num_friends and
+assumes we have already trained a simple model on `age`, `num_friends` and
 `viewed_minutes` features for a number of our users (a dummy model—
 named `'churn_model.skops'`—can be found in this file's directory).
 """
