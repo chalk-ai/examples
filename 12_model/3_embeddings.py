54492138afee1d65715324627a5bf78c80d77e90
Samuel Mignot <sjmignot@gmail.com>
2024-06-24 16:06:38 -0700

    revert to faq

--- 12_model/3_embeddings.py
+++ 12_model/3_embeddings.py
@@ -1,111 +1,99 @@
-from chalk import Validation, online
-from chalk.features import features, DataFrame, feature,  before_all, Vector, Primary
-from sentence_transformers import SentenceTransformer
+from chalk import Validation, online, embedding
+from chalk.features import features, DataFrame, feature, before_all, Vector, Primary, has_many
 
 
-global model
 
-
-@before_all
-def load_embedding_model():
-    global model
-    model = SentenceTransformer('intfloat/e5-small-v2')
-
-
-@features
-class Movie:
+@features(max_staleness='infinity')
+class FAQDocument:
     id: int
     title: str
-    director: str
-    year: int
-    description: str
-    rating: float = feature(min=0.0, max=10.0),
-    runtime: int = feature(min=0.0),
-    genres=set[str]
+    body: str
+    link: str
     embedding_text: str
-    embedding: Vector[384]
+    embedding: Vector = embedding(
+        input=lambda: FAQDocument.embedding_text,
+        provider="openai",
+        model="text-embedding-ada-002"
+    )
+
 
 @features
 class SearchQuery:
     query: Primary[str]
-    embedding: Vector[384]
+    max_runtime: int = None
+    embedding_text: str
+    embedding: Vector = embedding(
+        input=lambda: SearchQuery.embedding_text,
+        provider="openai",
+        model="text-embedding-ada-002"
+    )
+    faqs: DataFrame[FAQDocument] = has_many(
+        lambda: SearchQuery.embedding.is_near(
+            FAQDocument.embedding
+        )
+    )
+    response: str
 
 
 @online
-def get_movie_embedding_text(description: Movie.description, genres: Movie.genres, director: Movie.director) -> Movie.embedding_text:
-    genres_text = ", ".join(list(genres))
-    return f"passage: {description}. Directed by {director}. {genres_text}"
-
-@online
-def get_embedding(embedding_text: Movie.embedding_text) -> Movie.embedding:
-    return model.encode(embedding_text)
-
-@online
-def get_query_embedding(embedding_text: SearchQuery.query) -> SearchQuery.embedding:
-    return model.encode(f"query: {embedding_text}")
+def generate_query_embedding_text(query: SearchQuery.query) -> SearchQuery.embedding_text:
+    return f"query: {query}"
 
 
 @online
-def get_movies() -> DataFrame[Movie]:
+def get_movie_embedding_text(body: FAQDocument.body, title: FAQDocument.title) -> FAQDocument.embedding_text:
+    return f"passage: {title}. {body}"
+
+
+@online
+def generate_response(
+    # Query for the five most relevant documents, and select their links
+    nearest_faqs: SearchQuery.faqs[
+      FAQDocument.link,
+      :3
+    ]
+) -> SearchQuery.response:
+    return "\n".join(nearest_faqs[FAQDocument.link])
+
+
+@online
+def get_movies() -> DataFrame[
+    FAQDocument.id, FAQDocument.title, FAQDocument.body, FAQDocument.link]:
     return DataFrame([
-        Movie(
-            id=0,
-            title="High and Low",
-            year=1963,
-            director="Akira Kurosawa",
-            rating=8.4,
-            runtime=143,
-            genres={"Crime", "thriller"},
-            description="An executive of a Yokohama shoe company becomes a victim of extortion when his chauffeur's son is kidnapped by mistake and held for ransom."
-        ),
-        Movie(
+        FAQDocument(
             id=1,
-            title="Spirited Away",
-            year=2001,
-            director="Hayao Miyazaki",
-            rating=8.6,
-            runtime=135,
-            genres={"Animation", "Adventure", "Fantasy"},
-            description="During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches and spirits, and where humans are changed into beasts.",
+            title="What is the difference between the online store and the offline store?",
+            body="The online store is intended to store features for low-latency retrieval in online query. Typically, the online store is implemented using Redis, DynamoDB, or (in some cases) Postgres. The offline store is intended to store historical logs of all previously ingested or computed features. It is used to compute large historical training sets. It is typically implemented using BigQuery, Snowflake, or other data warehouses.",
+            link="https://docs.chalk.ai/docs/faq#what-is-the-difference-between-the-online-store-and-the-offline-store"
         ),
-        Movie(
-            id=2
-            title="Anatomy of a Fall",
-            year=2023,
-            director="Justine Triet"
-            rating=7.7,
-            runtime=152,
-            genres={"Crime", "Thriller"},
-            description="A woman is suspected of murder after her husband's death; their half-blind son faces a moral dilemma as the main witness."
+        FAQDocument(
+            id=2,
+            title="Can we do RBAC (Role Based Access Control) within Chalk?",
+            body="Yes! Within the dashboard you can assign roles with different permissions to different users. The default roles available are shown below.",
+            link="https://docs.chalk.ai/docs/faq#can-we-do-rbac-role-based-access-control-within-chalk"
         ),
-        Movie(
+        FAQDocument(
             id=3,
-            title="The Cruz Brothers and Miss Malloy",
-            year=1980,
-            director="Kathleen Collins",
-            rating=6.2,
-            runtime=54,
-            genres={"Drama"},
-            description="Three Puerto Rican brothers retreat to a town in New York following their father's death during a bank robbery. There they are hired by an elderly Irishwoman to renovate her house so she can throw one last house party."
+            title="What are the necessary steps for us to get Chalk in our system?",
+            body="Please reach out via your support channel and we’d be happy to walk you through how to get Chalk setup running on your cloud infrastructure!",
+            link="https://docs.chalk.ai/docs/faq#what-are-the-necessary-steps-for-us-to-get-chalk-in-our-system"
         ),
-        Movie(
+        FAQDocument(
             id=4,
-            title="In the Mood for Love",
-            year=2000,
-            director="Wong Kar Wai",
-            rating=8.1,
-            runtime=98,
-            genres={"Romance","Drama"},
-            description="Two neighbors form a strong bond after both suspect extramarital activities of their spouses. However, they agree to keep their bond platonic so as not to commit similar wrongs."
+            title="Does Chalk have a feature catalog?",
+            body="Yes! You can view all the features for all namespaces deployed in your environments, along with some metadata on recent activity and updates.",
+            link="https://docs.chalk.ai/docs/faq#does-chalk-have-a-feature-catalog"
         ),
-        Movie(
+        FAQDocument(
             id=5,
-            title="Mad Max: Fury Road",
-            year=2015,
-            director="George Miller",
-            rating=8.1,
-            runtime=120,
-            genres={"Action"}
-            description="In a post-apocalyptic wasteland, a woman rebels against a tyrannical ruler in search for her homeland with the aid of a group of female prisoners, a psychotic worshipper and a drifter named Max.",
+            title="Can I upload features into the online store with an API endpoint?",
+            body="Yes! In addition to streaming and scheduled bulk ingests of features, you can submit requests using the upload_features SDK endpoints to synchronously ingest features into the online or offline stores using API clients.",
+            link="https://docs.chalk.ai/docs/faq#can-i-upload-features-into-the-online-store-with-an-api-endpoint",
+        ),
+        FAQDocument(
+            id=6,
+            title="How are resources provisioned for my Chalk cluster, and can I modify the configuration?",
+            body="We have default resource configurations for general environments. You can modify the configuration for your project’s cloud resources by modifying the specs under Settings > Resources > Advanced Resource Configuration. You must hit Save and Apply Changes in order for your configuration changes to go through. If you are not sure how you should configure your cloud resources, please reach out to us in your support channel!",
+            link="https://docs.chalk.ai/docs/faq#how-are-resources-provisioned-for-my-chalk-cluster-and-can-i-modify-the-configuration"
         )
     ])
