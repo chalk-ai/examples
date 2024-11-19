b52a2a99d1518d63fcd678746a7e0e3dbd3ac8bf
Samuel Mignot <sjmignot@gmail.com>
2024-11-19 15:32:52 -0800

    fix

--- full_examples/dynamic_pricing/src/feature_sets.py
+++ full_examples/dynamic_pricing/src/feature_sets.py
@@ -1,9 +1,12 @@
+import datetime as dt
+
 from pydantic import BaseModel
 from src.datasources import kafka_stream
 
+import chalk.functions as F
 from chalk import online
-from chalk.features import features, feature, DataFrame, _
-from chalk.streams import stream, Windowed, windowed
+from chalk.features import DataFrame, FeatureTime, _, feature, features
+from chalk.streams import Windowed, stream, windowed
 
 
 @features
@@ -26,7 +29,7 @@ class Hotel:
         "30m",
         "1h",
         expression=_.hotel_interactions[
-            _.customer_id, _.transaction_time > _.chalk_window
+            _.customer_id, _.ts> _.chalk_window
         ].sum(),
         materialization={"bucket_duration": "15m"},
     )
@@ -35,7 +38,7 @@ class Hotel:
         "30m",
         "1h",
         expression=_.hotel_interactions[
-            _.customer_id, _.transaction_time > _.chalk_window
+            _.customer_id, _.ts> _.chalk_window
         ].approx_count_distinct(),
         materialization={"bucket_duration": "15m"},
     )
@@ -49,7 +52,10 @@ class Hotel:
     # We can run a sagemaker prediction directly from chalk on a set of encoded prediction
     # features
     pricing: bytes = feature(
-        expression=F.sagemaker_predict(F.string_to_bytes(_.features_csv))
+        expression=F.sagemaker_predict(
+            F.string_to_bytes(_.features_csv, encoding="utf-8"),
+            endpoint="hotel-model-endpoint",
+        ),
         # by setting a max staleness of 30m, we cache this feature and don't rerun prediction
         # if the pricing feature for a particular hotel has been computed in the last 30m
         max_staleness="30m",
@@ -59,6 +65,7 @@ class Hotel:
 @features
 class HotelInteraction:
     id: int
+    ts: FeatureTime
     customer_id: int
     hotel_id: Hotel.id
     view_time_s: float
@@ -66,18 +73,23 @@ class HotelInteraction:
 
 @online
 def prediction_to_csv(
-    mvt: Hotel.mean_view_time_s,
+    mvt_15m: Hotel.mean_view_time_s["15m"],
+    mvt_30m: Hotel.mean_view_time_s["30m"],
+    mvt_1h: Hotel.mean_view_time_s["1h"],
+    cdv_15m: Hotel.count_distinct_views["15m"],
+    cdv_30m: Hotel.count_distinct_views["30m"],
+    cdv_1h: Hotel.count_distinct_views["1h"],
     num_rooms: Hotel.num_rooms,
     location: Hotel.location,
-    cdv: Hotel.count_distinct_views,
 ) -> Hotel.features_csv:
-    return f'{mvt["15m"]},{mvt["30m"]},{mvt["1h"]},{num_rooms},{location},{cdv["15m"]},{cdv["30m"]},{cdv["1h"]}'
+    return f'{mvt_15m},{mvt_30m},{mvt_1h},{num_rooms},{location},{cdv_15m},{cdv_30m},{cdv_1h}'
 
 
 # Set up a stream message to continously update materialized aggregates
 class HotelInteractionMessage(BaseModel):
     id: int
     customer_id: int
+    ts: dt.datetime
     hotel_id: int
     view_time_s: float
 
@@ -90,6 +102,7 @@ def get_customer_hotel_interactions(
 ) -> HotelInteraction:
     return HotelInteraction(
         id=message.id,
+        ts=message.ts,
         customer_id=message.customer_id,
         hotel_id=message.hotel_id,
         view_time_s=message.view_time_s,
@@ -100,6 +113,7 @@ def get_customer_hotel_interactions(
 # stream aggregates and postgres features:
 if __name__ == "__main__":
     from chalk.client import ChalkClient
+
     client = ChalkClient()
     dynamic_price = client.query(
         input={
