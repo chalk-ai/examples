3008ea90f12c51cf7f174fe523df49feff468c2f
Samuel Mignot <sjmignot@gmail.com>
2025-01-08 16:30:33 -0800

    fix comment in example

--- 03_caching/2_lastest_value.py
+++ 03_caching/2_lastest_value.py
@@ -10,8 +10,9 @@ class User:
     id: int
     name: str
 
-    # Setting the maximum staleness to `infinity` means that you'll
-    # always receive the latest value of the feature.
+    # Setting the maximum staleness to `infinity` means that this
+    # value is calculated once and then read from the online store
+    # for subsequent requests.
     fico_score: int = feature(max_staleness="infinity")
 
 
@@ -22,10 +23,6 @@ def get_fico_score(name: User.name) -> User.fico_score:
 
 
 if __name__ == "__main__":
-    # This time, when the FICO score feature is requested,
-    # we'll receive the most recently computed value
-    # Infinite caching plays well with prefetching.
-    # See 7_prefetching.py for more detail
     ChalkClient().query(
         input={User.name: "Katherine Johnson"},
         output=[User.fico_score],
