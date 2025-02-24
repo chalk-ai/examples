a09cf2e986362b1e89c84ce9544a97f523d2f28b
Samuel Mignot <sjmignot@gmail.com>
2025-02-24 14:44:31 -0800

    remove tests

--- full_examples/image_processing/tests/test_website.py
+++
@@ -1,7 +0,0 @@
-from src.resolvers import get_html, get_images
-
-
-def test_website():
-    res = get_html("https://www.google.com")
-    get_images(res, "https://www.google.com")
-    breakpoint()
